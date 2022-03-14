c3b1ddc059f29b5a4b2149b9bd747d04ccdbc554
somyagupta2001 <somyagupta777@gmail.com>
2022-03-14 23:03:39 +0530

    modified app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import Homepage from "./Homepage/Homepage";
+import Electronics from "./Categories/Electronics";
+import Fashion from "./Categories/Fashion";
+import Grocery from "./Categories/Grocery";
+import Cart from "./Cart/Cart";
+import Signin from "./SigninNSignup/Signin";
+import Signup from "./SigninNSignup/Signup";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cart: [], //cart is an array of {product: productStructure, count: num} structures
+      price: 0,
+    };
+  }
+
+
+  addHandler = (product) => {
+    let currentState = {...this.state};
+    currentState.cart.push({ product: product, count: 1 });
+    currentState.price = currentState.price + product.price;
+    this.setState( currentState , () => console.log(this.state));
+  };
+
+  increaseHandler = (prod) => {
+    let currentState = {...this.state};
+    const index = currentState.cart.findIndex(
+      (elem) => elem.product.id === prod.id
+    );
+    currentState.cart[index].count++;
+    currentState.price = currentState.price + prod.price;
+    this.setState( currentState , () => console.log(this.state));
+  };
+
+  decreaseHandler = (prod) => {
+    let currentState = {...this.state};
+    const index = currentState.cart.findIndex(
+      (elem) => elem.product.id === prod.id
+    );
+    if (currentState.cart[index].count > 1) {
+      currentState.cart[index].count--;
+      currentState.price = currentState.price - prod.price;
+    }
+    else if (currentState.cart[index].count === 1){
+      currentState.cart.splice(index, 1);
+      currentState.price = currentState.price - prod.price;
+    }
+
+    this.setState( currentState , () => console.log(this.state));
+  };
+
+  returnCount = (prod) => {
+    let currentState = {...this.state};
+    const cartElem = currentState.cart.find(
+      (elem) => elem.product.id === prod.id
+    );
+    if (cartElem === undefined)
+      return 0;
+    console.log("CartElem",cartElem);
+      return cartElem.count;
+  }
+
+  render() {
+    return (
+      <div className="App">
         <Router>
           <Navbar />
           <div className="content">
@@ -33,7 +98,8 @@ function App() {
           </div>
         </Router>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
